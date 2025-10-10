2df0c52f45e49fe76f38baeded481030c7e48f54
David_Rabasco <451823.joan23@fje.edu>
2025-10-10 17:38:48 +0200

    Index creado

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Figuras geométricas</title>
+</head>
+<body>
+    <form>
+        <h1>Elige una figura</h1>
+        <select id="figura" style="max-width: 350px;">
+            <legend for="figura">Figuras geométricas</legend>
+            <option value="triangulo" name="triangulo" id="triangulo">Triángulo</option>
+            <br>
+            <option value="rectangulo" name="rectangulo" id="rectangulo">Rectángulo</option>
+            <br>
+            <option value="cuadrado" name="cuadrado" id="cuadrado">Cuadrado</option>
+            <br>
+            <option value="circulo" name="circulo" id="circulo">Círculo</option>
+            <br>
+        </select>
+    </form>
+</body>
+</html>
