7b2c48eb96166419f79a1eb66c940da3fd915a88
David_Rabasco <451823.joan23@fje.edu>
2025-10-15 11:45:48 +0200

    Creación de carpetas

--- pruebas.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'FiguraGeometrica.php';
-    include 'Triangulo.php';
-
-    $triangulo = new Triangulo("triangulo", 4, 5, 46);
-
-    echo "Area".$triangulo->calcularArea()."<br>";
-    echo "Perimetro".$triangulo->calcularPerimetro()."<br>";
-?>
